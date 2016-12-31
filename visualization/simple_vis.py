e628991c83d588000d05cfb8470367afec591463
diamondstone <ddiamondstone@gmail.com>
2016-12-30 17:36:25 -0800

    Minor reformatting for line length.

--- visualization/simple_vis.py
+++ visualization/simple_vis.py
@@ -6,7 +6,9 @@ import numpy as np
 
 
 def main():
-  data = np.genfromtxt('data/smj10.csv', delimiter=',', names=['wavelength', 'L_response', 'M_response', 'S_response'])
+  data = np.genfromtxt(
+      'data/smj10.csv', delimiter=',',
+      names=['wavelength', 'L_response', 'M_response', 'S_response'])
   plt.plot(data['wavelength'], data['L_response'], c='r')
   plt.plot(data['wavelength'], data['M_response'], c='g')
   plt.plot(data['wavelength'], data['S_response'], c='b')
