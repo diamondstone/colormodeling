9ec75daf772eefa4e5f91480af6b91abefede031
diamondstone <ddiamondstone@gmail.com>
2017-01-01 10:53:25 -0800

    Add unit testing for color_model.py, fix bugs, and some light refactoring.

--- models/color_model.py
+++ models/color_model.py
@@ -20,6 +20,42 @@ class Color(namedtuple('Color', ['l_response', 'm_response', 's_response'])):
   spectrum) may distinguish between identical 'colors'.
   """
 
+
+# Fudge-factor for numerical calculations. Better to return an approximate match for
+# a color which can't be exactly matched, rather than not return any match.
+_EPSILON = .001
+
+
+WAVELENGTH_RESPONSES = np.genfromtxt(
+  'data/smj10.csv', delimiter=',',
+  names=['wavelength', 'L_response', 'M_response', 'S_response'])
+
+
+def ColorFromWavelength(wavelength):
+  """Map a single wavelength to the tristimulus responses, assuming power = 1."""
+  if wavelength < 380 or wavelength > 770:
+    raise ValueError("%d is not a wavelength of visible light." % wavelength)
+  index = (wavelength - 380) // 5
+  lower_color = Color(WAVELENGTH_RESPONSES[index][1],
+                      WAVELENGTH_RESPONSES[index][2],
+                      WAVELENGTH_RESPONSES[index][3])
+  if wavelength == 770:
+    return lower_color
+  upper_color = Color(WAVELENGTH_RESPONSES[index + 1][1],
+                      WAVELENGTH_RESPONSES[index + 1][2],
+                      WAVELENGTH_RESPONSES[index + 1][3])
+  upper_power = (wavelength % 5) / 5
+  lower_power = 1-upper_power
+  return _Combine(lower_color, lower_power, upper_color, upper_power)
+
+
+def _Combine(color_A, power_A, color_B, power_B):
+  """Mix two colors with appropriate powers."""
+  return Color(color_A.l_response*power_A + color_B.l_response*power_B,
+               color_A.m_response*power_A + color_B.m_response*power_B,
+               color_A.s_response*power_A + color_B.s_response*power_B)
+
+
 class ColorModel(object):
   """Interface class for color models.
 
@@ -48,7 +84,7 @@ class ColorModel(object):
     raise NotImplementedError
 
   def Approximation(self, color):
-      """Find and the return closest representable color to the given one."""
+    """Find and the return closest representable color to the given one."""
     raise NotImplementedError
 
 
@@ -90,21 +126,21 @@ class ThreeAdditivePrimaryColorModel(ColorModel):
     if len(colors) != 3:
       raise ValueError('Must be given three primary colors.')
     self._colors = colors
-    self._encode_matrix = np.matrix(colors)
+    self._decode_matrix = np.matrix(colors).getT()
     try:
-      self._decode_matrix = self._encode_matrix.getI()
+      self._encode_matrix = self._decode_matrix.getI()
     except np.linalg.linalg.LinAlgError:
       raise ValueError('Given primaries must have linearly independent LMS response vectors.')      
 
   def _AlmostRepresentation(self, color):
-      """How much of the primaries would one need to combine to get the color.
-      Components may be negative for non-representable colors."""
-      # Need column vector.
-      self._encode_matrix * np.matrix(color).getT()
+    """How much of the primaries would one need to combine to get the color.
+    Components may be negative for non-representable colors."""
+    # Need column vector.
+    return self._encode_matrix * np.matrix(color).getT()
     
   def IsRepresentable(self, color):
     """Representable colors are positive linear combinations of primaries."""
-    return all(x > 0 for x in self._AlmostRepresentation(color))
+    return all(x > -_EPSILON for x in self._AlmostRepresentation(color))
 
   def Representation(self, color):
     """Map the given color to its representation within the model.
@@ -113,7 +149,15 @@ class ThreeAdditivePrimaryColorModel(ColorModel):
     """
     if not self.IsRepresentable(color):
       return None
-    return self._AlmostRepresentation(color)
+    return self.ApproximateRepresentation(color)
+
+  def ApproximateRepresentation(self, color):
+    """Map the given color to its representation within the model.
+    
+    Representations are three-dimensional numpy column vectors.
+    """
+    return np.maximum(self._AlmostRepresentation(color),
+                      np.matrix([0]*3).getT())
 
   def Color(self, representation):
     """Map the given representation to the corresponding color."""
@@ -123,10 +167,10 @@ class ThreeAdditivePrimaryColorModel(ColorModel):
 
   def Approximation(self, color):
     """Find and the return closest representable color to the given one."""
-    return self.Color(np.arraymax(self._AlmostRepresentation(color), [0]*3)
+    return self.Color(self.ApproximateRepresentation(color))
 
 
-def _Normalize(self, lms_responses):
+def _Normalize(lms_responses):
   """Given lms responses, return the normalized responses where sum = 1."""
   normalization_factor = 1.0 / sum(lms_responses)
   return [normalization_factor * response for response in lms_responses]
@@ -142,22 +186,19 @@ class TwoArbitraryWavelengthsColorModel(ColorModel):
   """
 
   def __init__(self):
-    # Read wavelength response curves into memory, and compute
-    # 1) the slope between the colors at the endpoints, and
-    # 2) the wavelength of the point furthest from that line.
-    self._wavelength_responses = np.genfromtxt(
-        'data/smj10.csv', delimiter=',',
-        names=['wavelength', 'L_response', 'M_response', 'S_response'])
-    self._normalized_wavelength
-    normalized_violet_response = _Normalize(self._wavelength_responses[0][1:4])
-    normalized_red_response = _Normalize(self._wavelength_responses[-1][1:4])
     # In the plane of normalized responses, the wavelength responses form an inverted
     # u shape. If you take any point inside this u, and draw a line through it
     # parallel to the line between the tips of the u, it will cross the u in exactly two places.
     # This gives an algorithm for finding two wavelengths which can be combined to match
     # some realizable LMS response.
+    # To simplify this computation, we store
+    # 1) the slope between the colors at the endpoints, and
+    # 2) the wavelength of the point furthest from that line.
+    normalized_violet_response = _Normalize(WAVELENGTH_RESPONSES[0][1:4])
+    normalized_red_response = _Normalize(WAVELENGTH_RESPONSES[-1][1:4])
     self._critical_slope = (normalized_red_response[0]-normalized_violet_response[0]) / (
         normalized_red_response[1]-normalized_violet_response[1])  
+    self._greenest_wavelength = 500  # FIXME
 
   def IsRepresentable(self, color):
     """Representable colors are perceptible colors, so this model can determine whether a
@@ -170,25 +211,6 @@ class TwoArbitraryWavelengthsColorModel(ColorModel):
     Representations are dictionaries {wavelength: power} with at most two keys.
     """
     raise NotImplementedError
-
-  def _Combine(color_A, power_A, color_B, power_B):
-    """Mix two colors with appropriate powers."""
-    return Color(color_A.l_resonse*power_A + color_B.l_resonse*power_B,
-                 color_A.m_resonse*power_A + color_B.m_resonse*power_B,
-                 color_A.s_resonse*power_A + color_B.s_resonse*power_B)
-
-  def _ColorOfSingleWavelength(self, wavelength):
-    """Map a single wavelength to the tristimulus responses, assuming power = 1."""
-    if wavelength < 380 or wavelength > 770:
-      raise ValueError("%d is not a wavelength of visible light." % wavelength)
-    index = (wavelength - 380) // 5
-    lower_color = Color(self._wavelength_responses[index][1:])
-    if wavelength == 770:
-      return lower_color
-    upper_color = Color(self._wavelength_responses[index + 1][1:])
-    upper_power = (wavelength % 5) / 5
-    lower_power = 1-upper_power
-    return self._Combine(lower_color, lower_power, upper_color, upper_power)
 
   def Color(self, representation):
     """Map the given representation to the corresponding color."""
@@ -234,20 +256,29 @@ class CIE1931ColorModel(ColorModel):
     """Find and the return closest representable color to the given one."""
     raise NotImplementedError
 
+
 # Standard RGB color model. Provides a decent model of what a standard RGB monitor can
 # display, and how it displays it.
-SRGB_RED = Color(0.0, 0.0, 1.0)  # TODO: find real LMS responses of sRGB red.
-SRGB_GREEN = Color(0.0, 1.0, 0.0)  # TODO: find real LMS responses of sRGB green.
-SRGB_BLUE = Color(1.0, 0.0, 0.0)  # TODO: find real LMS responses of sRGB blue.
+SRGB_RED = Color(0.6, 0.3, 0.1)  # TODO: find real LMS responses of sRGB red.
+SRGB_GREEN = Color(0.4, 0.5, 0.1)  # TODO: find real LMS responses of sRGB green.
+SRGB_BLUE = Color(0.2, 0.2, 0.6)  # TODO: find real LMS responses of sRGB blue.
 SRGB_PRIMARIES = (SRGB_RED, SRGB_GREEN, SRGB_BLUE)
-SRGB_COLOR_MODEL = ThreePrimaryAdditiveColorModel(SRGB_PRIMARIES)
+SRGB_COLOR_MODEL = ThreeAdditivePrimaryColorModel(SRGB_PRIMARIES)
+
+
+# Best possible RGB color model with actual colors of light.
+BEST_RGB_RED = ColorFromWavelength(700)
+BEST_RGB_GREEN = ColorFromWavelength(530)
+BEST_RGB_BLUE = ColorFromWavelength(400)
+BEST_RGB_PRIMARIES = (BEST_RGB_RED, BEST_RGB_GREEN, BEST_RGB_BLUE)
+BEST_RGB_COLOR_MODEL = ThreeAdditivePrimaryColorModel(SRGB_PRIMARIES)
 
 
 # "Ideal" RGB color model. Can represent non-real colors (colors which cannot be
 # perceived by looking at any mixture of wavelengths).
-IDEAL_RED = Color(0.0, 0.0, 1.0)
+IDEAL_RED = Color(1.0, 0.0, 0.0)
 IDEAL_GREEN = Color(0.0, 1.0, 0.0)
-IDEAL_BLUE = Color(1.0, 0.0, 0.0)
+IDEAL_BLUE = Color(0.0, 0.0, 1.0)
 IDEAL_PRIMARIES = (IDEAL_RED, IDEAL_GREEN, IDEAL_BLUE)
-IDEAL_COLOR_MODEL = ThreePrimaryAdditiveColorModel(IDEAL_PRIMARIES)
+IDEAL_COLOR_MODEL = ThreeAdditivePrimaryColorModel(IDEAL_PRIMARIES)
 
--- models/color_model_test.py
+++ models/color_model_test.py
@@ -10,13 +10,27 @@ import color_model
 MAUVE = color_model.Color(.4, .3, .5)
 
 class IdealColorModelTest(unittest.TestCase):
-  def testRepresentation_Mauve(self):
-    self.assertTrue(np.array_equal(np.matrix('.5; .3; .4'),
-                             color_model.IDEAL_COLOR_MODEL.Representation(MAUVE)))
+  def testRepresentation_mauve_ideal(self):
+    np.testing.assert_array_almost_equal(
+        np.matrix('.4; .3; .5'),
+        color_model.IDEAL_COLOR_MODEL.Representation(MAUVE))
 
-  def testColor_MauveRepresentation(self):
-    self.assertEquals(MAUVE, color_model.IDEAL_COLOR_MODEL.Color(
-                                 color_model.IDEAL_COLOR_MODEL.Representation(MAUVE)))
+  def testColorAndRepresentationAreInverses_mauve_ideal(self):
+    np.testing.assert_array_almost_equal(
+      MAUVE,
+      color_model.IDEAL_COLOR_MODEL.Color(
+          color_model.IDEAL_COLOR_MODEL.Representation(MAUVE)))
+
+  def testRepresentation_srgbRed_srgb(self):
+    np.testing.assert_array_almost_equal(
+        np.matrix('1.0; 0.0; 0.0'),
+        color_model.SRGB_COLOR_MODEL._AlmostRepresentation(color_model.SRGB_RED))
+
+  def testColorAndRepresentationAreInverses_mauve_srgb(self):
+    np.testing.assert_array_almost_equal(
+        MAUVE,
+        color_model.SRGB_COLOR_MODEL.Color(
+            color_model.SRGB_COLOR_MODEL.Representation(MAUVE)))
 
 if __name__ == '__main__':
   unittest.main()
